cdbcf42580665736bf014c1c44b6878fdbf379b1
PanupongDev <5635512110@psu.ac.th>
2018-11-24 20:59:27 +0700

    crate Pizza

---
+++ FactoryPattern/pizzaStore/productClass/chicagostyle/index.ts
@@ -0,0 +1,73 @@
+import Pizza from '../../Pizza';
+
+export class ChicagoStyleCheesePizza extends Pizza {
+    public prepare() {
+
+    }
+
+    public bake() {
+
+    }
+
+    public cut() {
+
+    }
+
+    public box() {
+
+    }
+}
+
+export class ChicagoStylePepporoniPizza extends Pizza {
+    public prepare() {
+
+    }
+
+    public bake() {
+
+    }
+
+    public cut() {
+
+    }
+
+    public box() {
+        
+    }
+}
+
+export class ChicagoStyleClamPizza extends Pizza {
+    public prepare() {
+
+    }
+
+    public bake() {
+
+    }
+
+    public cut() {
+
+    }
+
+    public box() {
+        
+    }
+}
+
+export class ChicagoStyleVeggiePizza extends Pizza {
+    public prepare() {
+
+    }
+
+    public bake() {
+
+    }
+
+    public cut() {
+
+    }
+
+    public box() {
+        
+    }
+}
---
+++ FactoryPattern/pizzaStore/productClass/newyorkstyle/index.ts
@@ -0,0 +1,73 @@
+import Pizza from '../../Pizza';
+
+export class NyStyleCheesePizza extends Pizza {
+    public prepare() {
+
+    }
+
+    public bake() {
+
+    }
+
+    public cut() {
+
+    }
+
+    public box() {
+
+    }
+}
+
+export class NyStylePepporoniPizza extends Pizza {
+    public prepare() {
+
+    }
+
+    public bake() {
+
+    }
+
+    public cut() {
+
+    }
+
+    public box() {
+        
+    }
+}
+
+export class NyStyleClamPizza extends Pizza {
+    public prepare() {
+
+    }
+
+    public bake() {
+
+    }
+
+    public cut() {
+
+    }
+
+    public box() {
+        
+    }
+}
+
+export class NyStyleVeggiePizza extends Pizza {
+    public prepare() {
+
+    }
+
+    public bake() {
+
+    }
+
+    public cut() {
+
+    }
+
+    public box() {
+        
+    }
+}
