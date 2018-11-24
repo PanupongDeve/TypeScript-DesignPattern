bc152655472b1d3cc2017766cca3d7ca4162dd88
PanupongDev <5635512110@psu.ac.th>
2018-11-24 21:15:12 +0700

    create NYStylePizzaStore class

---
+++ FactoryPattern/pizzaStore/createClass/NYStylePizzaStore.ts
@@ -0,0 +1,26 @@
+import PizzaStore from '../PizzaStore';
+import Pizza from '../Pizza';
+
+import {
+    NyStyleCheesePizza,
+    NyStyleClamPizza,
+    NyStylePepporoniPizza,
+    NyStyleVeggiePizza
+} from '../productClass/newyorkstyle';
+
+export class NYStylePizzaStore extends PizzaStore {
+    public createPizza(type: string): Pizza {
+        let pizza: Pizza;
+        if (type === 'cheese') {
+            pizza = new NyStyleCheesePizza();
+        } else if (type === 'pepperoni') {
+            pizza = new NyStyleClamPizza();
+        } else if (type === 'clam') {
+            pizza = new NyStylePepporoniPizza();
+        } else if (type === 'veggie') {
+            pizza = new NyStyleVeggiePizza();
+        }
+
+        return pizza;
+    }
+}
