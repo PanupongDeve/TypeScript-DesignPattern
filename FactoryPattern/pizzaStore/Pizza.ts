054205a2017fd51d74dca4fc1303df302c99d1f0
PanupongDev <5635512110@psu.ac.th>
2018-11-24 21:30:34 +0700

    create method in pizza

--- FactoryPattern/pizzaStore/Pizza.ts
+++ FactoryPattern/pizzaStore/Pizza.ts
@@ -1,9 +1,27 @@
 
 export default abstract class Pizza {
+    name: string;
+    dough: string;
+    sauce: string;
+    toppings: string[] = [];
 
-    abstract prepare();
-    abstract bake();
-    abstract cut();
-    abstract box();
+    public prepare(): void {
+        console.log(`Preparing ${this.name}`);
+        console.log(`Tossing dough...`);
+        console.log(`Adding sauce`);
+        console.log(`Adding Toppins:`);
+        this.toppings.forEach(topping => {
+            console.log(`  ${topping}`);
+        });
+    }
+    public bake(): void {
+        console.log('Bake for 25 minutes at 350');
+    }
+    public cut(): void {
+        console.log('Cutting the pizza into diagonal slices');
+    }
+    public box(): void {
+        console.log('Place pizza in offical PizzaStore box');
+    }
 
 }
